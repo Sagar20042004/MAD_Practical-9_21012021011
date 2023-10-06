4f467472aae9b5578cd75d6e21175015441e0865
student <student@gnu.ac.in>
2023-10-06 08:59:26 +0530

    pr9

--- app/src/main/java/com/example/mad_practical_9_21012021011/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021011/SplashActivity.kt
@@ -1,11 +1,53 @@
 package com.example.mad_practical_9_21012021011
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(),Animation.AnimationListener {
+
+    lateinit var AnimationLogo : AnimationDrawable
+    lateinit var logoImage : ImageView
+    lateinit var logoAnimation : Animation
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+     val logoImage : ImageView = findViewById(R.id.imageview)
+        logoImage.setBackgroundResource(R.drawable.logo_animation_list)
+        AnimationLogo = logoImage.background as AnimationDrawable
+
+
+        logoAnimation = AnimationUtils.loadAnimation(this , R.anim.twin_animation)
+        logoAnimation.setAnimationListener(this)
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if (hasFocus){
+            logoImage.startAnimation(logoAnimation)
+        }
+        else{
+            AnimationLogo.stop()
+        }
+        super.onWindowFocusChanged(hasFocus)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+        //animationAnd maintent no use kari bija ma redirect karvanu che
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
+    }
+
 }
+
